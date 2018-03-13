6ff2c5ab3a5d2bd200074b73203865b69d9f1715
mfuentealba@larrainvial.com <http://quilimari.larrainvial.com>
2018-03-13 19:57:21 -0300

    sgsdfg

--- btcbnb.js
+++ btcbnb.js
@@ -10,7 +10,7 @@ const cluster = require('cluster');
 
 
 
-binance.websockets.depthCache(["BNBBTC"], function(symbol, depth) {
+binance.websockets.depthCache(["BTCUSDT"], function(symbol, depth) {
 	let max = 10; // Show 10 closest orders only
 	let bids = binance.sortBids(depth.bids, max);
 	let asks = binance.sortAsks(depth.asks, max);
@@ -19,14 +19,14 @@ binance.websockets.depthCache(["BNBBTC"], function(symbol, depth) {
 	console.log("bids", bids);
 	console.log("ask: "+binance.first(asks));
 	console.log("bid: "+binance.first(bids));*/
-	fnCruce('bin', asks, 'BNBBTC', 'buy');
-	fnCruce('bin', bids, 'BNBBTC_', 'sell');
+	fnCruce('bin', asks, 'BTCUSDT', 'buy');
+	fnCruce('bin', bids, 'BTCUSDT_', 'sell');
 });
 
 
 
 
-binance.websockets.depthCache(["ONTBNB"], function(symbol, depth) {
+binance.websockets.depthCache(["NEOBTC"], function(symbol, depth) {
 	let max = 10; // Show 10 closest orders only
 	let bids = binance.sortBids(depth.bids, max);
 	let asks = binance.sortAsks(depth.asks, max);
@@ -35,11 +35,11 @@ binance.websockets.depthCache(["ONTBNB"], function(symbol, depth) {
 	console.log("bids", bids);
 	console.log("ask: "+binance.first(asks));
 	console.log("bid: "+binance.first(bids));*/
-	fnCruce('bin', asks, 'ONTBNB', 'buy');
-	fnCruce('bin', bids, 'ONTBNB_', 'sell');
+	fnCruce('bin', asks, 'NEOBTC', 'buy');
+	fnCruce('bin', bids, 'NEOBTC_', 'sell');
 });
 
-binance.websockets.depthCache(["ONTBTC"], function(symbol, depth) {
+binance.websockets.depthCache(["NEOUSDT"], function(symbol, depth) {
 	let max = 10; // Show 10 closest orders only
 	let bids = binance.sortBids(depth.bids, max);
 	let asks = binance.sortAsks(depth.asks, max);
@@ -48,8 +48,8 @@ binance.websockets.depthCache(["ONTBTC"], function(symbol, depth) {
 	console.log("bids", bids);
 	console.log("ask: "+binance.first(asks));
 	console.log("bid: "+binance.first(bids));*/
-	fnCruce('bin', bids, 'ONTBTC', 'sell');
-	fnCruce('bin', asks, 'ONTBTC_', 'buy');
+	fnCruce('bin', bids, 'NEOUSDT', 'sell');
+	fnCruce('bin', asks, 'NEOUSDT_', 'buy');
 });
 
 
@@ -73,31 +73,31 @@ function fnCruce(orig, data, currencyPair, op){
 	
 	validacionDatos[currencyPair].data = data;
 	//console.log(currencyPair);
-	if(validacionDatos['BNBBTC'] && validacionDatos['ONTBNB'] && validacionDatos['ONTBTC']
-	&& validacionDatos['BNBBTC_'] && validacionDatos['ONTBTC_'] && validacionDatos['ONTBNB_']
+	if(validacionDatos['BTCUSDT'] && validacionDatos['NEOBTC'] && validacionDatos['NEOUSDT']
+	&& validacionDatos['BTCUSDT_'] && validacionDatos['NEOUSDT_'] && validacionDatos['NEOBTC_']
 	&& countOrd == 0){
 		console.log("ORDER: " + countOrd);
-		var qty1 = 0.002 / binance.first(validacionDatos['BNBBTC'].data);
-		qty1 = Number(qty1.toFixed(2))
-		var px1 = binance.first(validacionDatos['BNBBTC'].data);
+		var qty1 = 11.9 / binance.first(validacionDatos['BTCUSDT'].data);
+		qty1 = Number(qty1.toFixed(6))
+		var px1 = binance.first(validacionDatos['BTCUSDT'].data);
 		var amount1 = qty1 * px1 * (1 - 0.001 / 0.999);
-		//console.log(binance.first(validacionDatos['BNBBTC_'].data));
+		//console.log(binance.first(validacionDatos['BTCUSDT_'].data));
 		console.log("px: " + px1 + ", qty: " + qty1 + ", amount: " + amount1);
-		var qty2 = qty1 * (1 - 0.001 / 0.999) / binance.first(validacionDatos['ONTBNB'].data);//lowestAsk;
-		qty2 = Number(qty2.toFixed(0))
-		var px2 = binance.first(validacionDatos['ONTBNB'].data);//lowestAsk;
+		var qty2 = qty1 * (1 - 0.001 / 0.999) / binance.first(validacionDatos['NEOBTC'].data);//lowestAsk;
+		qty2 = Number(qty2.toFixed(2))
+		var px2 = binance.first(validacionDatos['NEOBTC'].data);//lowestAsk;
 		var amount2 = qty2 * px2 * (1 - 0.001 / 0.999);
 		console.log("px: " + px2 + ", qty: " + qty2 + ", amount: " + amount2);
 		
 		
-		var qty3 = qty2 * (1 - 0.001 / 0.999);// * binance.first(validacionDatos['ONTBNB_'].data);
-		qty3 = Number(qty3.toFixed(0));
-		var px3 = binance.first(validacionDatos['ONTBTC'].data);
+		var qty3 = qty2 * (1 - 0.001 / 0.999);// * binance.first(validacionDatos['NEOBTC_'].data);
+		qty3 = Number(qty3.toFixed(3));
+		var px3 = binance.first(validacionDatos['NEOUSDT'].data);
 		var amount3 = qty3 * px3 * (1 - 0.001 / 0.999);
 		console.log("px: " + px3 + ", qty: " + qty3 + ", amount: " + amount3);
 		var result = amount3;
 		console.log(result);
-		result = result - 0.002;
+		result = result - 11.9;
 		console.log("RES: " + ((qty1 * px1) - amount3));
 		
 		
@@ -107,37 +107,38 @@ function fnCruce(orig, data, currencyPair, op){
 		
 		
 		
-		var qty4 = 2; /// binance.first(validacionDatos['BNBBTC_'].data)
-		qty4 = Number(qty4.toFixed(2));
-		var px4 = binance.first(validacionDatos['BNBBTC_'].data);
+		var qty4 = 11.9 / binance.first(validacionDatos['BTCUSDT_'].data)
+		qty4 = Number(qty4.toFixed(6));
+		var px4 = binance.first(validacionDatos['BTCUSDT_'].data);
 		var amount4 = qty4 * px4 * (1 - 0.001 / 0.999);
-		//console.log(binance.first(validacionDatos['BNBBTC_'].data));
+		//console.log(binance.first(validacionDatos['BTCUSDT_'].data));
 		console.log("px: " + px4 + ", qty: " + qty4 + ", amount: " + amount4);
-		var qty5 = amount4 / binance.first(validacionDatos['ONTBTC_'].data);//lowestAsk;
-		qty5 = Number(qty5.toFixed(0));	
-		var px5 = binance.first(validacionDatos['ONTBTC_'].data);//lowestAsk;
+		var qty5 = amount4 / binance.first(validacionDatos['NEOUSDT_'].data);//lowestAsk;
+		qty5 = Number(qty5.toFixed(3));	
+		var px5 = binance.first(validacionDatos['NEOUSDT_'].data);//lowestAsk;
 		var amount5 = qty5 * px5 * (1 - 0.001 / 0.999);
 		console.log("px: " + px5 + ", qty: " + qty5 + ", amount: " + amount5);
-		var qty6 = qty5 * (1 - 0.001 / 0.999);// * binance.first(validacionDatos['ONTBNB_'].data);
+		var qty6 = qty5 * (1 - 0.001 / 0.999);// * binance.first(validacionDatos['NEOBTC_'].data);
 		qty6 = Number(qty6.toFixed(2));
-		var px6 = binance.first(validacionDatos['ONTBNB_'].data);
+		var px6 = binance.first(validacionDatos['NEOBTC_'].data);
 		var amount6 = qty6 * px6 * (1 - 0.001 / 0.999);
 		console.log("px: " + px6 + ", qty: " + qty6 + ", amount: " + amount6);
 		var result2 = amount6;
 		console.log(result2);
-		result2 = result2 - 2;
+		result2 = result2 - qty4;
 		console.log("RES2: " + ((qty4) - amount6));
 		
 		if(result > 0.00001){
-			countOrd++;
+			
 			console.log('\u0007');
 			console.log('qty1: ' + qty1);
-			binance.MarketBuy("BNBBTC", qty1, px1, {type:'LIMIT'}, (error, response) => {
+			binance.buy("BTCUSDT", qty1, px1, {type:'LIMIT'}, (error, response) => {
+				countOrd++;
 			  console.log("Limit Buy response", response);
 			  console.log("order id: " + response.orderId);
 			  if(error && error.body){
 				console.log(error.body);
-				fsLauncher.appendFileSync('./bin2.txt', 'BNBBTC\n' + JSON.stringify(error.body) + " \n", (err) => {
+				fsLauncher.appendFileSync('./bin2.txt', 'BTCUSDT\n' + JSON.stringify(error.body) + " \n", (err) => {
 						if (err) throw err;
 							////console.log('The "data to append" was appended to file!');
 						});  
@@ -150,12 +151,13 @@ function fnCruce(orig, data, currencyPair, op){
 			});
 			
 			console.log('qty2: ' + qty2);
-			binance.MarketBuy("ONTBNB", qty2, px2, {type:'LIMIT'}, (error, response) => {
+			binance.buy("NEOBTC", qty2, px2, {type:'LIMIT'}, (error, response) => {
+				countOrd++;
 			  console.log("Limit Buy response", response);
 			  console.log("order id: " + response.orderId);
 			  if(error && error.body){
 				console.log(error.body);
-				fsLauncher.appendFileSync('./bin2.txt', 'BNBBTC\n' + JSON.stringify(error.body) + " \n", (err) => {
+				fsLauncher.appendFileSync('./bin2.txt', 'NEOBTC\n' + JSON.stringify(error.body) + " \n", (err) => {
 						if (err) throw err;
 							////console.log('The "data to append" was appended to file!');
 						});  
@@ -167,12 +169,13 @@ function fnCruce(orig, data, currencyPair, op){
 			});
 			
 			console.log('qty3: ' + qty3);
-			binance.MarketSell("ONTBTC", qty3, px3, {type:'LIMIT'}, (error, response) => {
+			binance.sell("NEOUSDT", qty3, px3, {type:'LIMIT'}, (error, response) => {
+				countOrd++;
 			  console.log("Limit Buy response", response);
 			  console.log("order id: " + response.orderId);
 			  if(error && error.body){
 				console.log(error.body);
-				fsLauncher.appendFileSync('./bin2.txt', 'BNBBTC\n' + JSON.stringify(error.body) + " \n", (err) => {
+				fsLauncher.appendFileSync('./bin2.txt', 'NEOUSDT\n' + JSON.stringify(error.body) + " \n", (err) => {
 						if (err) throw err;
 							////console.log('The "data to append" was appended to file!');
 						});  
@@ -186,14 +189,15 @@ function fnCruce(orig, data, currencyPair, op){
 		
 		
 		if(result2 > 0.00001){
-			countOrd++;
+			
 			console.log('\u0007');
-			binance.sell("BNBBTC", qty4, px4, {type:'LIMIT'}, (error, response) => {
+			binance.sell("BTCUSDT", qty4, px4, {type:'LIMIT'}, (error, response) => {
+			  countOrd++;
 			  console.log("Limit Buy response", response);
 			  console.log("order id: " + response.orderId);
 			  if(error && error.body){
 				console.log(error.body);
-				fsLauncher.appendFileSync('./bin2.txt', 'BNBBTC\n' + JSON.stringify(error.body) + " \n", (err) => {
+				fsLauncher.appendFileSync('./bin2.txt', 'BTCUSDT\n' + JSON.stringify(error.body) + " \n", (err) => {
 						if (err) throw err;
 							////console.log('The "data to append" was appended to file!');
 						});  
@@ -205,12 +209,13 @@ function fnCruce(orig, data, currencyPair, op){
 			});
 			
 			
-			binance.buy("ONTBTC", qty5, px5, {type:'LIMIT'}, (error, response) => {
+			binance.buy("NEOUSDT", qty5, px5, {type:'LIMIT'}, (error, response) => {
+			  countOrd++;
 			  console.log("Limit Buy response", response);
 			  console.log("order id: " + response.orderId);
 			 if(error && error.body){
 				console.log(error.body);
-				fsLauncher.appendFileSync('./bin2.txt', 'BNBBTC\n' + JSON.stringify(error.body) + " \n", (err) => {
+				fsLauncher.appendFileSync('./bin2.txt', 'NEOUSDT\n' + JSON.stringify(error.body) + " \n", (err) => {
 						if (err) throw err;
 							////console.log('The "data to append" was appended to file!');
 						});  
@@ -222,12 +227,13 @@ function fnCruce(orig, data, currencyPair, op){
 			});
 			
 			
-			binance.sell("ONTBNB", qty6, px6, {type:'LIMIT'}, (error, response) => {
+			binance.sell("NEOBTC", qty6, px6, {type:'LIMIT'}, (error, response) => {
+			  countOrd++;	
 			  console.log("Limit Buy response", response);
 			  console.log("order id: " + response.orderId);
 			  if(error && error.body){
 				console.log(error.body);
-				fsLauncher.appendFileSync('./bin2.txt', 'BNBBTC\n' + JSON.stringify(error.body) + " \n", (err) => {
+				fsLauncher.appendFileSync('./bin2.txt', 'NEOBTC\n' + JSON.stringify(error.body) + " \n", (err) => {
 						if (err) throw err;
 							////console.log('The "data to append" was appended to file!');
 						});  
